9137bd95c2e4b1b463a4b93bc4a3386cce9f8006
RangerDevv <66392962+RangerDevv@users.noreply.github.com>
2026-07-17 21:08:58 -0400

    Fixed overflow errors for username and string as well as handeled negative ID numbers

--- main.c
+++ main.c
@@ -1,5 +1,5 @@
 // #include <cstdint>
-#include <stdbool.h>
+#include <stdbool.h>C
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -10,8 +10,8 @@
 typedef struct
 {
     __uint32_t id;
-    char username[COLUMN_USER_SIZE];
-    char email[COLUMN_EMAIL_SIZE];
+    char username[COLUMN_USER_SIZE + 1];
+    char email[COLUMN_EMAIL_SIZE + 1];
 } Row;
 
 typedef struct
@@ -30,6 +30,8 @@ typedef enum
 typedef enum
 {
     PREPARE_SUCCESS,
+    PREPARE_STRING_TOO_LONG,
+    PREPARE_NEGATIVE_ID,
     PREPARE_UNRECOGNIZED_STATEMENT,
     PREPARE_SYNTAX_ERROR
 } PrepareResult;
@@ -113,6 +115,8 @@ void* row_slot(Table* table, __uint32_t row_num)
 
 void print_prompt() {printf("db >");}
 
+void print_row(Row* row) {printf("%d , %s , %s \n",row->id,row->username,row->email);}
+
 void read_input(InputBuffer* input_buffer)
 {
     ssize_t bytes_read = getline(&(input_buffer->buffer), &(input_buffer->buffer_length),stdin); // allocate memory to store the input
@@ -146,18 +150,47 @@ MetaCommandResult do_meta_command(InputBuffer* input_buffer)
     }
 }
 
+PrepareResult prepare_insert(InputBuffer* input_buffer, Statement* statement)
+{
+    statement->type = STATEMENT_INSERT;
+
+    char* keyword  = strtok(input_buffer->buffer, " ");
+    char* id_string = strtok(NULL, " ");
+    char* username  = strtok(NULL, " ");
+    char* email    = strtok(NULL, " ");
+
+    if (id_string==NULL || username==NULL || email==NULL)
+    {
+        return PREPARE_SYNTAX_ERROR;
+    }
+
+    int id = atoi(id_string); // atoi --> ASCII to Int
+    if (id < 0)
+    {
+        return PREPARE_NEGATIVE_ID;
+    }
+    if (strlen(username) > COLUMN_USER_SIZE)
+    {
+        return PREPARE_STRING_TOO_LONG;
+    }
+    if (strlen(email) > COLUMN_EMAIL_SIZE)
+    {
+        return PREPARE_STRING_TOO_LONG;
+    }
+
+    statement->row_to_insert.id = id;
+    strcpy(statement->row_to_insert.username, username);
+    strcpy(statement->row_to_insert.email, email);
+
+    return PREPARE_SUCCESS;
+
+}
+
 PrepareResult prepare_statement(InputBuffer* input_buffer, Statement* statement)
 {
     if (strncmp(input_buffer->buffer, "insert", 6)==0)
     {
-        statement->type = STATEMENT_INSERT;
-        int args_assigned = sscanf(
-            input_buffer->buffer, "insert %d %s %s", &(statement->row_to_insert.id), statement->row_to_insert.username,statement->row_to_insert.email);
-        if (args_assigned<3)
-        {
-            return PREPARE_SYNTAX_ERROR;
-        }
-        return PREPARE_SUCCESS;
+        return prepare_insert(input_buffer, statement);
     }
     if (strcmp(input_buffer->buffer, "select")==0)
     {
@@ -249,12 +282,29 @@ int main(int argc, char *argv[])
         switch (prepare_statement(input_buffer, &statement))
         {
             case (PREPARE_SUCCESS):
-            break;
+                break;
+            case (PREPARE_SYNTAX_ERROR):
+                printf("Syntax error could not parse statement.");
+                continue;
+            case (PREPARE_STRING_TOO_LONG):
+                printf("String too long!");
+                continue;
+            case (PREPARE_NEGATIVE_ID):
+                printf("Cannot have a negative ID number");
+                continue;
             case (PREPARE_UNRECOGNIZED_STATEMENT):
-            printf("Unrecognized keyword `%s'\n", input_buffer->buffer);
-            continue;
+                printf("Unrecognized keyword `%s'\n", input_buffer->buffer);
+                continue;
         }
-        execute_statement(&statement);
-        printf("Executed!");
+        switch (execute_statement(&statement, table))
+        {
+            case (EXECUTE_SUCCESS):
+                printf("Executed!");
+                break;
+            case (EXECUTE_TABLE_FULL):
+                printf("Table full!");
+            break;
+
+        }
     }
 }
